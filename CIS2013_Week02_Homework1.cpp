fb26395791785f1b0fca80b9605843163f9fc15a
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-01-30 01:55:51 -0600

    Week02_Homework1
    Calculator

---
+++ CIS2013_Week02_Homework1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		int num1, num2, num3;
+		char opr;
+		char question = 'y';
+		bool keep_running = true;
+		
+		while (keep_running)
+		{
+		
+			cout << "Enter your first number";
+			cin >> num1;
+		
+			cout << "Enter you second number";
+			cin >> num2;
+		
+			cout << "Enter your operation";
+			cin >> opr;
+		
+				if (opr == '+')
+				{ 
+					num3=num1+num2;
+				 
+				}
+				if (opr == '-')
+				{
+					num3=num1-num2;
+				
+				}
+				else if (opr == '*')
+				{
+					num3=num1*num2;
+				
+				}
+				else if (opr == '/')
+				{
+					num3=num1/num2;
+				 
+				}
+			
+				cout << "Here is your Solution:" << num3 << endl;
+				cout << "Run again (Y/N): ";
+				cin >> question;
+				
+				if(question != 'y') (keep_running = false);
+		}
+				
+			
+		
+		
+		
+	return 0;
+}
